40cbfbb6764454ccf7662e96c0686ccf3b506add
Astriditiya Laila Nur Fadilah <lailafadilah383@gmail.com>
2023-12-20 11:11:24 +0700

    update dockerfile

---
+++ build_push_shipping_service.sh
@@ -0,0 +1,7 @@
+docker build -t shipping-service:latest .
+
+docker images
+
+docker tag shipping-service:latest lailafadilah383/shipping-service:latest
+
+docker push lailafadilah383/shipping-service:latest
